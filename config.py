33bfd874bb5ff1abad7c34548b57e144524e28b8
pradanaadn <pradanaadn21@gmail.com>
2025-11-20 10:41:23 +0800

    feat: add configuration management for S3 credentials using pydantic-settings

---
+++ config.py
@@ -0,0 +1,37 @@
+from typing import Tuple, Type
+
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+
+class S3Secret(BaseModel):
+    region: str
+    access_key_id: str
+    secret_access_key: str
+
+
+class YamlSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        yaml_file="secret.yaml", extra="ignore", case_sensitive=False
+    )
+    s3: S3Secret
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (YamlConfigSettingsSource(settings_cls),)
+
+
+yaml_settings = YamlSettings()
+print(yaml_settings.model_dump())
